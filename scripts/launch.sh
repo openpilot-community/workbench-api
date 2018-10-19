aeafadcefbfad90fbaa60e5020e0442150cfef4c
Chase Higgins <chasehiggins@Chases-MacBook-Pro.local>
2018-10-18 18:50:56 -0700

    did some housekeeping, haven't tested yet

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
 function launch {
-  # no cpu rationing for now
-  # echo 0-3 > /dev/cpuset/background/cpus
-  # echo 0-3 > /dev/cpuset/system-background/cpus
-  # echo 0-3 > /dev/cpuset/foreground/boost/cpus
-  # echo 0-3 > /dev/cpuset/foreground/cpus
-  # echo 0-3 > /dev/cpuset/android/cpus
+
   export PYTHONPATH="/data/openpilot"
 
   # start manager
